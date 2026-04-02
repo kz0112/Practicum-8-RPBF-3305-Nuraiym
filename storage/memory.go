f88c2f4e2aa44d5056a4e699899a614dfb76f144
Ваше Имя <_email@example.com>
2026-04-03 01:57:57 +0500

    first commit

--- storage/memory.go
+++
@@ -1,8 +0,0 @@
-package storage
-
-import "Private-medical-clinic.backend/models"
-
-var Books = []models.Book{
-	{ID: 1, Title: "Go Basics", Author: "John Doe", Category: "Programming"},
-	{ID: 2, Title: "REST API", Author: "Jane Smith", Category: "Backend"},
-}
